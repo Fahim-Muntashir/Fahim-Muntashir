6dc24e107a5a357450a06da24211b643ccb58835
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-08-28 11:19:14 +0600

    added many comp

--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -1,11 +1,15 @@
+import About from './About';
 import Contact from './Contact';
 import Home from './Home';
+import Review from './Review';
 
 const page = () => {
     return (
         <div>
-            <Home></Home>
+            <Home></Home> <About></About>
+           <Review></Review>
             <Contact></Contact>
+           
         </div>
     );
 };
