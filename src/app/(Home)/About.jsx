8298c991d34d64b259163bc8ad603113cbdf91fb
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-02 10:26:00 +0600

    added many section with my photo

--- src/app/(Home)/About.jsx
+++ src/app/(Home)/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
                 <div>
                     <h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">Html to FullStack Webdeveloper</h3>
-                    <p className="mt-3 text-lg dark:text-gray-400">Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.</p>
+                    <p className="mt-3 text-lg dark:text-gray-400">The Journey was startted when I am in class 8 the covid time in 2020.I Say a tutorial video on a tv channel html teach by Jhankar Mahbub .Then I am Curious this webdevelopment and start the journey</p>
                     <div className="mt-12 space-y-12">
                         <div className="flex">
                             <div className="flex-shrink-0">
@@ -55,7 +55,7 @@ const About = () => {
                     </div>
                 </div>
                 <div aria-hidden="true" className="mt-10 lg:mt-0">
-                    <img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+                    <img src="https://i.ibb.co/5kdd88H/Dark-Green-Modern-Photo-centric-Student-Part-time-Volunteer-Resume-Mobile-First-Presentation-2.jpg" alt="" className="mx-auto rounded-lg shadow-lg w-80 dark:bg-gray-500" />
                 </div>
             </div>
             <div>
--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -1,15 +1,19 @@
 import About from './About';
+import Blog from './Faq';
 import Contact from './Contact';
 import Home from './Home';
 import Review from './Review';
+import Skill from './Skill';
 
 const page = () => {
     return (
         <div>
-            <Home></Home> <About></About>
-           <Review></Review>
+            <Home></Home>
+            <About></About>
+            <Skill></Skill>
+            <Review></Review>
             <Contact></Contact>
-           
+            <Blog></Blog>
         </div>
     );
 };
