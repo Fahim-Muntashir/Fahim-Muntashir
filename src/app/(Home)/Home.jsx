d7b9922e44a7da5d40638f760a1ea185a44943ce
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-02 08:48:40 +0600

    added about and home

--- src/app/(Home)/Home.jsx
+++ src/app/(Home)/Home.jsx
@@ -3,16 +3,16 @@ const Home = () => {
     return (
         <section className="bg-gray-800 text-gray-100">
 	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-28 lg:flex-row lg:justify-between">
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm  lg:text-left">
+		<div className="flex flex-col justify-center p-6 text-center rounded-sm  lg:text-left my-40">
 			<h1 className="text-5xl font-bold sm:text-6xl">Hello, I am <br />
 			Fahim Muntashir
 			</h1>
-			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-				<br className="hidden md:inline lg:hidden"/>turpis pulvinar, est scelerisque ligula sem
+			<p className="mt-6 mb-8 text-lg sm:mb-12">Professional Full Stack Webdeveloper
+				<br className="hidden md:inline lg:hidden"/> with 3 years of Amazing Experience
 			</p>
 			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Suspendisse</a>
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100">Malesuada</a>
+				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 text-gray-900">Download CV</a>
+				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded border-gray-100">About Me</a>
 			</div>
 		</div>
 		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
