53c7ca5f618aae7ef1e6aa66db585df814ec2701
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-10-24 18:57:44 +0600

    home page update 2.0

--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -1,23 +1,23 @@
-import About from './About';
-import Blog from './Faq';
-import Contact from './Contact';
-import Home from './Home';
-import Review from './Review';
-import Skill from './Skill';
-import Projects from './Projects';
+import About from "./About";
+import Blog from "./Faq";
+import Contact from "./Contact";
+import Home from "./Home";
+import Review from "./Review";
+import Skill from "./Skill";
+import Projects from "./Projects";
 
 const page = () => {
-    return (
-        <div>
-            <Home></Home>
-            <About></About>
-            <Skill></Skill>
-            <Projects></Projects>
-            <Review></Review>
-            <Contact></Contact>
-            <Blog></Blog>
-        </div>
-    );
+  return (
+    <div>
+      <Home></Home>
+      <About></About>
+      <Skill></Skill>
+      <Projects></Projects>
+      <Review></Review>
+      <Contact></Contact>
+      <Blog></Blog>
+    </div>
+  );
 };
 
 export default page;
