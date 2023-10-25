6376c3981913b653753e4b04912be64a51678034
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-10-25 09:42:29 +0600

    portfolio added

--- src/app/(Home)/About.jsx
+++ src/app/(Home)/About.jsx
@@ -1,67 +1,148 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <section className="dark:bg-gray-800 dark:text-gray-100">
-        <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
-            <div>
-                <h2 className="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">About My Journey</h2>
-                <p className="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">My Development Journey Share Now I am ,on here</p>
-            </div>
-            <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
-                <div>
-                    <h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">Html to FullStack Webdeveloper</h3>
-                    <p className="mt-3 text-lg dark:text-gray-400">My journey into web development began during the COVID-19 pandemic in 2020 when I was in the 8th grade. It all started when I stumbled upon a tutorial video on a TV channel, where Jhankar Mahbub was teaching HTML. Intrigued by the world of web development, I decided to embark on this exciting journey.</p>
-                    <div className="mt-12 space-y-12">
-                        <div className="flex">
-                            <div className="flex-shrink-0">
-                                <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                            <div className="ml-4">
-                                <h4 className="text-lg font-medium leadi dark:text-gray-50">Journey 2020 - 2021 After this</h4>
-                                <p className="mt-2 dark:text-gray-400">After watching Jhankar Mahbubs video, he recommended starting my learning journey with W3Schools. I heeded his advice and began my exploration by diving into HTML and CSS on the platform. There were moments when I felt a bit overwhelmed and apprehensive, but I didnt let that deter me. To reinforce my understanding, I turned to coding tutorials on YouTube, which helped me grasp concepts in HTML, CSS, JavaScript, some PHP, and Bootstrap. This multi-pronged approach to learning has been invaluable in building my web development skills.</p>
-                            </div>
-                        </div>
-                        <div className="flex">
-                            <div className="flex-shrink-0">
-                                <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                            <div className="ml-4">
-                                <h4 className="text-lg font-medium leadi dark:text-gray-50">After That whats happen</h4>
-                                <p className="mt-2 dark:text-gray-400">This year, I embarked on a journey by enrolling in a course at Programming Hero, where I delved into the world of MERN Stack development. In the initial days of this course, I encountered some challenging moments, and there were days filled with frustration as I grappled with coding. However, its all part of the learning process, and I continued to persevere.</p>
-                            </div>
-                        </div>
-                        <div className="flex">
-                            <div className="flex-shrink-0">
-                                <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                            <div className="ml-4">
-                                <h4 className="text-lg font-medium leadi dark:text-gray-50">The Main Journey Start</h4>
-                                <p className="mt-2 dark:text-gray-400">
-                                        After taking Programming Hero  course and dedicating several months to hard work, I diligently honed my skills. Through countless hours on YouTube and online platforms, I crafted various MERN (MongoDB, Express.js, React, Node.js) projects. As a result, I have not only boosted my confidence but also solidified my belief that I am now a proficient programmer. I feel confident in my abilities and believe that I am  well-prepared for a job in this field.
-                                <a className='text-blue-500'>  Read More...</a>
-                                        </p>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <section className="dark:bg-gray-800 dark:text-gray-100">
+      <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+        <div>
+          <h2 className="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">
+            About My Journey
+          </h2>
+          <p className="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">
+            My Development Journey Share Now I am ,on here
+          </p>
+        </div>
+        <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+          <div>
+            <h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">
+              Html to FullStack Webdeveloper
+            </h3>
+            <p className="mt-3 text-lg dark:text-gray-400">
+              My journey into web development began during the COVID-19 pandemic
+              in 2020 when I was in the 8th grade. It all started when I
+              stumbled upon a tutorial video on a TV channel, where Jhankar
+              Mahbub was teaching HTML. Intrigued by the world of web
+              development, I decided to embark on this exciting journey.
+            </p>
+            <div className="mt-12 space-y-12">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
                 </div>
-                <div aria-hidden="true" className="mt-10 lg:mt-0">
-                    <img src="https://i.ibb.co/TtDCMwf/Dark-Green-Modern-Photo-centric-Student-Part-time-Volunteer-Resume-Mobile-First-Presentation-4.jpg" alt="" className="mx-auto rounded-lg shadow-lg w-80 dark:bg-gray-500" />
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leadi dark:text-gray-50">
+                    Journey 2020 - 2021 After this
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">
+                    After watching Jhankar Mahbubs video, he recommended
+                    starting my learning journey with W3Schools. I heeded his
+                    advice and began my exploration by diving into HTML and CSS
+                    on the platform. There were moments when I felt a bit
+                    overwhelmed and apprehensive, but I didnt let that deter me.
+                    To reinforce my understanding, I turned to coding tutorials
+                    on YouTube, which helped me grasp concepts in HTML, CSS,
+                    JavaScript, some PHP, and Bootstrap. This multi-pronged
+                    approach to learning has been invaluable in building my web
+                    development skills.
+                  </p>
                 </div>
+              </div>
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leadi dark:text-gray-50">
+                    After That whats happen
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">
+                    This year, I embarked on a journey by enrolling in a course
+                    at Programming Hero, where I delved into the world of MERN
+                    Stack development. In the initial days of this course, I
+                    encountered some challenging moments, and there were days
+                    filled with frustration as I grappled with coding. However,
+                    its all part of the learning process, and I continued to
+                    persevere.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leadi dark:text-gray-50">
+                    The Main Journey Start
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">
+                    After taking Programming Hero course and dedicating several
+                    months to hard work, I diligently honed my skills. Through
+                    countless hours on YouTube and online platforms, I crafted
+                    various MERN (MongoDB, Express.js, React, Node.js) projects.
+                    As a result, I have not only boosted my confidence but also
+                    solidified my belief that I am now a proficient programmer.
+                    I feel confident in my abilities and believe that I am
+                    well-prepared for a job in this field.
+                    <a className="text-blue-500"> Read More...</a>
+                  </p>
+                </div>
+              </div>
             </div>
-            {/* <div>
+          </div>
+          <div aria-hidden="true" className="mt-0 lg:mt-0">
+            <img
+              src="https://i.ibb.co/q9FTksF/ee012c95-1c60-49bb-aa8e-977fc056ab2c.jpg"
+              alt=""
+              className="mx-auto h-full shadow-lg w-80 dark:bg-gray-500"
+            />
+          </div>
+        </div>
+        {/* <div>
                 <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
                     <div className="lg:col-start-2">
                         <h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">Eam nibh gloriatur ex</h3>
@@ -113,9 +194,9 @@ const About = () => {
                     </div>
                 </div>
             </div> */}
-        </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default About;
